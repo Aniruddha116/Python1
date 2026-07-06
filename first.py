4dbd8dfc0f27acfd4f0328b8a92d7f076588641c
Aniruddha <aniruddhakunduargho@gmail.com>
2026-07-06 19:41:59 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+age=3;
+old=False;
+print(type(old));
+print(type(age));
+if 5>2:
+    print("argho");
+if 4>1:
+         print("ani")
+         print("kc")
+
+print("heello")
+print("python is fun")
+print("accpet")
+print('aksssss', end=" ")
+print("this is new")
+print("i am ", 34 ,"years old")
+
